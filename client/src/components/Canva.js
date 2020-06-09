cc74b299ade39485d97cb118801f626f87d7a0fc
Victor Oliveira <vob@cesar.school>
2020-06-09 20:44:23 -0300

    p5 working

---
+++ client/src/components/Canva.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class Canva extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Im a component</h1>
+      </div>
+    )
+  }
+}
+
+export default Canva
