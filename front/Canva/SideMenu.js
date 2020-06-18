25be19f736ae6d8f41b46c849859222d98c3ea47
Henrique Joaquim <42215737+concrete-kite@users.noreply.github.com>
2020-06-17 21:46:01 -0300

    Add files via upload

---
+++ front/Canva/SideMenu.js
@@ -0,0 +1,115 @@
+import React, { Component } from "react";
+
+class SideMenu extends Component {
+  state = {
+    colorButtons: [
+      {
+        id: 1,
+        name: "black",
+        style: {
+          flex: 1,
+          border: "none",
+          borderRadius: "4px",
+          background: "#000000",
+          padding: "20px 2.5em",
+          cursor: "pointer",
+        },
+      },
+      {
+        id: 2,
+        name: "red",
+        style: {
+          flex: 1,
+          border: "none",
+          borderRadius: "4px",
+          background: "#ff0000",
+          padding: "20px 2.5em",
+          cursor: "pointer",
+        },
+      },
+      {
+        id: 3,
+        name: "blue",
+        style: {
+          flex: 1,
+          border: "none",
+          borderRadius: "4px",
+          background: "#1a1aff",
+          padding: "20px 2.5em",
+          cursor: "pointer",
+        },
+      },
+      {
+        id: 4,
+        name: "yellow",
+        style: {
+          border: "none",
+          borderRadius: "4px",
+          background: "#ffff00",
+          padding: "20px 2.5em",
+          cursor: "pointer",
+        },
+      },
+      {
+        id: 5,
+        name: "green",
+        style: {
+          flex: 1,
+          border: "none",
+          borderRadius: "4px",
+          background: "#1aff1a",
+          padding: "20px 2.5em",
+          cursor: "pointer",
+        },
+      },
+      {
+        id: 6,
+        name: "pink",
+        style: {
+          flex: 1,
+          border: "none",
+          borderRadius: "4px",
+          background: "#ff4dd2",
+          padding: "20px 2.5em",
+          cursor: "pointer",
+        },
+      },
+      {
+        id: 7,
+        name: "brown",
+        style: {
+          flex: 1,
+          border: "none",
+          borderRadius: "4px",
+          background: "#663300",
+          padding: "20px 2.5em",
+          cursor: "pointer",
+        },
+      },
+      {
+        id: 8,
+        name: "background",
+        style: {
+          flex: 1,
+          border: "none",
+          borderRadius: "4px",
+          background: "#f0f0f0",
+          padding: "20px 2.5em",
+          cursor: "pointer",
+        },
+      },
+    ],
+  };
+
+  render() {
+    return this.state.colorButtons.map((item) => (
+      <button
+        style={item.style}
+        key={item.id}
+        onClick={this.props.changeColor.bind(this, item.style.background)}
+      ></button>
+    ));
+  }
+}
+
+export default SideMenu;
---
+++ front/Canva/sketch.js
@@ -0,0 +1,148 @@
+const sketch = (p) => {
+
+    const url = "b449f2c643c1.ngrok.io"
+    const connection = new WebSocket("ws://" + url);
+    const button = document.querySelector("#send");
+
+    let canvas;
+    let canvaColor = "#f0f0f0";
+    let height = 400;
+    let width = 700;
+    let color;
+    let del = 0;
+    let rst = 0;
+    let rstFlag = false;
+    let delFlag = false;
+    let moves = [];
+    let lastMove = [];
+    let drawMode = true;
+
+
+    let fun = {
+      clear: function() {
+        p.background(canvaColor);
+        delFlag = !delFlag;
+      },
+      drawColor: function() {
+        p.fill(color);
+        p.noStroke();
+        let x = Math.floor(p.mouseX);
+        let y  = Math.floor(p.mouseY);
+        if(!fun.eraserMode()){
+          p.rect(x, y, 3, 3);
+          fun.getLastMove();
+        }else{
+          p.ellipse(x, y, 40, 40);
+        }
+      },
+      delLastMove: function() {
+        let a = moves.pop();
+        try {
+          a.map(item => {
+            p.fill(canvaColor);
+            p.rect(item.x, item.y, 3, 3);
+            connection.send(JSON.stringify({mouseX: item.x, mouseY: item.y, color: canvaColor}));
+          });
+        }catch (err){
+
+        }
+        rstFlag = !rstFlag;
+      },
+      getLastMove: function() {
+        let pos  = {};
+        pos['x'] = Math.floor(p.mouseX);
+        pos['y'] = Math.floor(p.mouseY);
+        lastMove.push(pos);
+      },
+      inCanva: function() {
+        // I don't like terciary ops.
+        let x = p.mouseX;
+        let y = p.mouseY;
+        if(canvas && (x > 0 && x <= width) && (y > 0 && y <= height)){
+          return true
+        }else{
+          return false
+        }
+      },
+      eraserMode: function() {
+        if(color === canvaColor){
+          return true
+        }
+        return false
+      }
+
+    }
+
+    connection.onopen = (event) => {
+      console.log("WebSocket is open now.");
+    };
+
+    connection.onclose = (event) => {
+      console.log("WebSocket is closed now.");
+    };
+
+    connection.onerror = (event) => {
+      console.error("WebSocket error observed:", event);
+    };
+
+    connection.onmessage = (event) => {
+      const { mouseX, mouseY, color} = JSON.parse(event.data);
+      p.fill(color);
+      p.rect(mouseX, mouseY, 3, 3);
+    };
+
+    p.mouseReleased = () => {
+      if (fun.inCanva() && !fun.eraserMode()) {
+        console.log()
+        moves.push(lastMove);
+        lastMove = [];
+      }
+    }
+
+    //Adicionar bound esquedo e superior no if
+    p.mouseDragged = () => {
+      if (fun.inCanva()) {
+        fun.drawColor();
+        let mouseX = p.mouseX;
+        let mouseY = p.mouseY;
+        connection.send(JSON.stringify({mouseX, mouseY, color}));
+      }
+      return false;
+    }
+    
+    p.setup = () => {
+      canvas = p.createCanvas(width, height);
+      p.background(canvaColor);
+      p.noStroke();
+      p.frameRate(60);
+
+    }
+    
+    p.draw = () => {
+      if(drawMode){
+        if(delFlag){
+          fun.clear();
+        }else if(rstFlag){
+          fun.delLastMove();
+        }
+      }else{
+        // let new pixel = com func
+        // draw new pixel 
+      }
+    }
+
+    // o Wrapper usa essa função para pegar props : )
+    p.myCustomRedrawAccordingToNewPropsHandler = (props) => {
+        color = props.color;
+        if(del !== props.del){
+            delFlag = !delFlag;
+            del = props.del;
+        }
+        if(rst !== props.rst){
+          rstFlag = !rstFlag;
+          rst = props.rst;
+        }
+    }
+}
+
+export default sketch;
